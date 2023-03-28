0ad5d00c1f7bf61beb53e473658ced74c44626b2
Badr1669 <TU4000321@taibahu.edu.sa>
2023-03-28 06:27:54 +0300

    Add controler Company

---
+++ TadarbApp/TadarbApp/TadarbProject/Controllers/CompanyController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TadarbProject.Data;
+using TadarbProject.Models;
+using TadarbProject.Models.ViewModels;
+using TEST2.Services;
+
+namespace TadarbProject.Controllers
+{
+    //yazeed edit
+    //badr modify
+    //abdulhadi
+    public class CompanyController : Controller
+    {
+        private readonly AppDbContext _DbContext;
+        private readonly IWebHostEnvironment _WebHostEnvironment;
+        private readonly IEmailSender _emailSender;
+       
+
+        public IActionResult Index()
+        {
+
+
+            return View();
+        }
+
+        public IActionResult AddSpecialities()
+        {
+
+
+            return View();
+        }
+
+        public IActionResult Addbranches()
+        {
+
+
+            return View();
+        }
+
+
+
+
+    }
+}
