a4d6c20639f7b804eee4cdea524f7dd60c620d0d
Badr1669 <TU4000321@taibahu.edu.sa>
2023-03-30 13:52:01 +0300

    Add Unversity Contrroler

---
+++ TadarbApp/TadarbApp/TadarbProject/Controllers/UniversityController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TadarbProject.Data;
+using TadarbProject.Models;
+using TadarbProject.Models.ViewModels;
+using TEST2.Services;
+
+namespace TadarbProject.Controllers
+{
+    //yazeed edit
+    //badr modify
+    //abdulhadi
+    public class UniversityController : Controller
+    {
+        private readonly AppDbContext _DbContext;
+        private readonly IWebHostEnvironment _WebHostEnvironment;
+        private readonly IEmailSender _emailSender;
+       
+
+        public IActionResult Index()
+        {
+
+
+            return View();
+        }
+
+        public IActionResult AddColleges()
+        {
+
+
+            return View();
+        }
+
+
+
+
+
+
+    }
+}
