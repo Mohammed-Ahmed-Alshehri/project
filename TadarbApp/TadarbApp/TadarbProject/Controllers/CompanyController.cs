b72550faa778a82616448df04b41a6a01b50ece6
Abdulhadi <Abeeed2000@icloud.com>
2023-04-03 05:33:34 +0300

    validation in company adduser page

--- TadarbApp/TadarbApp/TadarbProject/Controllers/CompanyController.cs
+++ TadarbApp/TadarbApp/TadarbProject/Controllers/CompanyController.cs
@@ -75,95 +75,125 @@ namespace TadarbProject.Controllers
             return View();
         }
 
-
-        //[HttpPost]
-        //public IActionResult AddUsers(EmployeeVM employeeVM)
-        //{
-
-        //    if (employeeVM == null)
-        //    {
-
-        //        return View();
-
-        //    }
-
-        //    var user = new UserAcount
-        //    {
-        //        UserEmail = employeeVM.userAcount.UserEmail,
-        //        UserPassword = employeeVM.userAcount.UserPassword,
-        //        FullName = employeeVM.userAcount.FullName,
-        //        Phone = employeeVM.userAcount.Phone,
-        //        City_CityId = 1,
-        //        UserType = "Branch_Admin",
-        //        ActivationStatus = "Active"
+        #region
 
 
 
+        [HttpGet]
+        public IActionResult EmailExists(string? Email)
+        {
+            if (Email == null)
+            {
+                return Json(new { Exists = false });
+            }
 
-        //    };
+            var item = _DbContext.UserAcounts.Where(item => item.UserEmail.Equals(Email)).FirstOrDefault();
 
-        //    _DbContext.UserAcounts.Add(user);
-
-        //    _DbContext.SaveChanges();
-
-        //    int RUserId = _DbContext.UserAcounts.Where(item => item.UserEmail == user.UserEmail).First().UserId;
-
-        //    ////var DEP = new Department
-        //    ////{
-
-        //    ////    DepartmentName = "قسم ادارة الفروع",
-        //    ////    Organization_OrganizationId = 4,
-        //    ////    Responsible_UserId = 7,
-
-
-
-        //    ////};
-
-        //    //_DbContext.Departments.Add(DEP);
-
-        //    //_DbContext.SaveChanges();
-
-        //    int DEPId = _DbContext.Departments.Where(item => item.Organization_OrganizationId == 4 && item.DepartmentName.Equals("قسم ادارة الفروع")).First().DepartmentId;
-
-        //    var EMP = new Employee
-        //    {
-
-        //        Department_DepartmentId = DEPId,
-        //        Job_JobId = 1,
-        //        SSN = employeeVM.employee.SSN,
-        //        UserAccount_UserId = RUserId,
+            if (item == null)
+            {
+                return Json(new { Exists = false });
+            }
 
 
 
-        //    };
-
-        //    _DbContext.Employees.Add(EMP);
-
-        //    _DbContext.SaveChanges();
-
-        //    return View();
-        //}
+            return Json(new { Exists = true });
+        }
 
 
-
-
-        //#region
-        //public IActionResult GetAllEMP()
-        //{
-        //    var OR = _DbContext.Organizations.Where(item => item.OrganizationId == 4).FirstOrDefault();
-
-        //    var  dpt = _DbContext.Departments.Where(item => item.Organization_OrganizationId == OR.OrganizationId && item.DepartmentName.Equals("قسم ادارة الفروع")).FirstOrDefault();
-
-        //    var emp = _DbContext.Employees.Where(item => item.Department_DepartmentId == dpt.DepartmentId).Include(item => item.userAcount);
-
-
-        //    //var UserAcountList = _DbContext.UserAcounts.Where(item => item.UserId == );
-
-
-
-        //    return Json(new { data = emp });
     }
+
+    #endregion
+
+
+
+    //[HttpPost]
+    //public IActionResult AddUsers(EmployeeVM employeeVM)
+    //{
+
+    //    if (employeeVM == null)
+    //    {
+
+    //        return View();
+
+    //    }
+
+    //    var user = new UserAcount
+    //    {
+    //        UserEmail = employeeVM.userAcount.UserEmail,
+    //        UserPassword = employeeVM.userAcount.UserPassword,
+    //        FullName = employeeVM.userAcount.FullName,
+    //        Phone = employeeVM.userAcount.Phone,
+    //        City_CityId = 1,
+    //        UserType = "Branch_Admin",
+    //        ActivationStatus = "Active"
+
+
+
+
+    //    };
+
+    //    _DbContext.UserAcounts.Add(user);
+
+    //    _DbContext.SaveChanges();
+
+    //    int RUserId = _DbContext.UserAcounts.Where(item => item.UserEmail == user.UserEmail).First().UserId;
+
+    //    ////var DEP = new Department
+    //    ////{
+
+    //    ////    DepartmentName = "قسم ادارة الفروع",
+    //    ////    Organization_OrganizationId = 4,
+    //    ////    Responsible_UserId = 7,
+
+
+
+    //    ////};
+
+    //    //_DbContext.Departments.Add(DEP);
+
+    //    //_DbContext.SaveChanges();
+
+    //    int DEPId = _DbContext.Departments.Where(item => item.Organization_OrganizationId == 4 && item.DepartmentName.Equals("قسم ادارة الفروع")).First().DepartmentId;
+
+    //    var EMP = new Employee
+    //    {
+
+    //        Department_DepartmentId = DEPId,
+    //        Job_JobId = 1,
+    //        SSN = employeeVM.employee.SSN,
+    //        UserAccount_UserId = RUserId,
+
+
+
+    //    };
+
+    //    _DbContext.Employees.Add(EMP);
+
+    //    _DbContext.SaveChanges();
+
+    //    return View();
+    //}
+
+
+
+
+    //#region
+    //public IActionResult GetAllEMP()
+    //{
+    //    var OR = _DbContext.Organizations.Where(item => item.OrganizationId == 4).FirstOrDefault();
+
+    //    var  dpt = _DbContext.Departments.Where(item => item.Organization_OrganizationId == OR.OrganizationId && item.DepartmentName.Equals("قسم ادارة الفروع")).FirstOrDefault();
+
+    //    var emp = _DbContext.Employees.Where(item => item.Department_DepartmentId == dpt.DepartmentId).Include(item => item.userAcount);
+
+
+    //    //var UserAcountList = _DbContext.UserAcounts.Where(item => item.UserId == );
+
+
+
+    //    return Json(new { data = emp });
 }
+
 //#endregion
 //}
 //}
